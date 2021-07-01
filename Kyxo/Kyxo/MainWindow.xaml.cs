1ee0dfb730233207f8c2cc614669f17b0305b52c
kyxodevelopment <84624315+kyxodevelopment@users.noreply.github.com>
2021-07-01 10:08:42 +0700

    Add files via upload

---
+++ Kyxo/Kyxo/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Kyxo
+{
+    public partial class MainWindow : Window
+    {
+        // Kyxo Softworks (c) 2020-2021
+        /* - Make sure to credit us if u were to use this template.
+           - We cleared the source so gtfo skids 
+           - The user interface is resizeable, we left all the styles so feel free to use it, but dont forget to credit or gonna get exposed */
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
